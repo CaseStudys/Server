6e3477152edeabae992618a341485da062fed2d6
sotatsu kitajima <halosakakitajima@gmail.com>
2021-12-03 19:39:43 +0900

    テストで出品のpostAPIを書いてみました。参考にしてください。

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root",
-  database: "express_db",
+  database: "hal_motor_auction",
 });
 
 module.exports.mysql_connection = db;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const cors = require("cors");
 const employeeRouter = require("./routers/employee/route.js");
+const exhibitRouter = require("./routers/exhibit/route.js");
 
 app.use(express.json());
 app.use(cors());
 app.use("/", employeeRouter);
+app.use("/", exhibitRouter);
 
 app.listen(PORT, console.log(`✋ Server Start:ポート番号${PORT}番 ✋`));
