765a012ce6fdb39d02d91a31a575ae0912fd7f12
Maedakoyo <periperi512@gmail.com>
2021-12-14 17:11:14 +0900

    test/route.js  修正

--- routers/test/route.js
+++ routers/test/route.js
@@ -7,21 +7,22 @@ router.route(`${TEST_END_POINT}`).get((req, res) => {
   const employeeId = req.params.employee_id;
   const path = __dirname;
   console.log("path!!!", path);
-  db.mysql_connection.connect((err) => {
-    const placeholder = [employeeId];
-    db.mysql_connection.query(
-      "select * from employees where employee_id = ?",
-      placeholder,
-      (err, result) => {
-        if (err) {
-          return res.status(500).json({ message: err });
-        }
-        return res.status("200").render("test.ejs", {
-          values: { values: "valuesテスト" },
-        });
-      }
-    );
-  });
+  res.status("200").render("test.ejs");
+  // db.mysql_connection.connect((err) => {
+  //   const placeholder = [employeeId];
+  //   db.mysql_connection.query(
+  //     "select * from employees where employee_id = ?",
+  //     placeholder,
+  //     (err, result) => {
+  //       if (err) {
+  //         return res.status(500).json({ message: err });
+  //       }
+  //       return res.status("200").render("test.ejs", {
+  //         values: { values: "valuesテスト" },
+  //       });
+  //     }
+  //   );
+  // });
 });
 
 module.exports = router;
