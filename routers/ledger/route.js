5a9c689a78ca696732890463c7dddf0b4721dd61
Maedakoyo <periperi512@gmail.com>
2021-12-21 15:19:00 +0900

    ・ledger(GET)追加 ・テスト用ejs修正

--- routers/ledger/route.js
+++ routers/ledger/route.js
@@ -1,1 +1,27 @@
 /*/ledger*/
+const express = require("express");
+const db = require("../../db");
+const router = express.Router();
+const LEDGER_END_POINT = "/ledger";
+
+
+//台帳管理画面を表示しプロジェクト一覧を渡すGETのAPI
+router.route(LEDGER_END_POINT).get(async (req, res) => {
+    //DB処理
+    db.mysql_connection.connect((err) => {
+      db.mysql_connection.query(
+        "SELECT * FROM projects;",
+        (err, result) => {
+          if (err) {
+            return res.status(500).json({ code: 500, message: err });
+          }
+          return res.status("200").render("emp_ledger_management.ejs", {values: result});
+          //テスト用
+          //return res.status("200").render("get_test.ejs", {values: result});
+        }
+      );
+    });
+  });
+
+
+module.exports = router;
