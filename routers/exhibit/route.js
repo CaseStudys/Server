ba2ebf2adab8ffd6f600aa0f9b2298a35863a1ea
sotatsu kitajima <halosakakitajima@gmail.com>
2021-12-24 13:57:12 +0900

    fix #33 bid(GET)追加

--- routers/exhibit/route.js
+++ routers/exhibit/route.js
@@ -4,10 +4,8 @@ const db = require("../../db"); //db処理で使う。
 const router = express.Router(); //api定義で使う。これを使えばファイル分割してapi作れる。
 const EXHIBIT_END_POINT = "/exhibit"; //APIのURL
 
-
 //出品する中古車両のID(car_id)を受け取り、出品登録するPOSTのAPI
 router.route(EXHIBIT_END_POINT).post(async (req, res) => {
-
   //db登録に必要なデータをbodyから取得。
   const body = req.body;
   const carId = body.car_id;
@@ -18,14 +16,16 @@ router.route(EXHIBIT_END_POINT).post(async (req, res) => {
   const insertCar = "INSERT INTO exhibits(car_id,start_date) VALUE(?,?);";
   //register_flgたてる
   const updateRegister = "UPDATE cars SET register_flg = 1 WHERE car_id = ?;";
+
   //まだ出品登録されていない車両情報を取得
-  const selectCars = "SELECT car_id,picture_path,type_name,purchace_price FROM cars WHERE register_flg = 0;";
+  const selectCars =
+    "SELECT car_id,picture_path,type_name,purchace_price FROM cars WHERE register_flg = 0;";
 
   //DB処理
   db.mysql_connection.connect((err) => {
     const placeholder = [carId, startDate, carId];
     db.mysql_connection.query(
-      insertCar+updateRegister+selectCars,
+      insertCar + updateRegister + selectCars,
       placeholder,
       (err, result) => {
         if (err) {
@@ -36,8 +36,6 @@ router.route(EXHIBIT_END_POINT).post(async (req, res) => {
     );
   });
 });
-
-
 
 //(従業員)登録者出品ページを表示するGETのAPI
 //出品登録されていない車両データをすべて返す
@@ -50,7 +48,7 @@ router.route(EXHIBIT_END_POINT).get(async (req, res) => {
         if (err) {
           return res.status(500).json({ code: 500, message: err });
         }
-        return res.status("200").render("emp_exhibit.ejs", {values: result});
+        return res.status("200").render("emp_exhibit.ejs", { values: result });
         //テスト用
         //return res.status("200").json(result);//値確認
         //return res.status("200").render("get_test.ejs", {values: result});//ejs確認
